dcb4eccdc68af6d2588773f51d83f944a8e2e3e5
bgkillas <bgkillas.mw@gmail.com>
2023-06-26 15:56:01 -0400

    put readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# timer
+
+a simple cli timer app SIGUSR2 to start and SIGUSR1 to increment
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout, Write};
+use libc::{c_int, signal, SIGUSR1, SIGUSR2};
+use std::time::{Duration, Instant};
+use std::thread::sleep;
+static mut SIGNAL_RECEIVED:bool = false;
+static mut START:bool = false;
+extern "C" fn handle_signal(_:c_int)
+{
+    unsafe {
+        SIGNAL_RECEIVED = true;
+    }
+}
+extern "C" fn handle_start(_:c_int)
+{
+    unsafe {
+        START = true;
+    }
+}
+fn main()
+{
+    unsafe {
+        signal(SIGUSR1, handle_signal as usize);
+    }
+    unsafe {
+        signal(SIGUSR2, handle_start as usize);
+    }
+    loop
+    {
+        sleep(Duration::from_millis(1));
+        unsafe {
+            if START
+            {
+                break;
+            }
+        }
+    }
+    let watch = Instant::now();
+    let mut n;
+    loop
+    {
+        n = watch.elapsed().as_nanos();
+        unsafe {
+            print!("\x1B[2K\x1B[1G{:02}:{:02}:{:02}.{:09}{}",
+                   n / (3_600_000_000_000),
+                   (n / (60_000_000_000)) % 60,
+                   (n / (1_000_000_000)) % 60,
+                   n % 1_000_000_000,
+                   if SIGNAL_RECEIVED
+                   {
+                       SIGNAL_RECEIVED = false;
+                       "\n"
+                   }
+                   else
+                   {
+                       ""
+                   });
+        }
+        stdout().flush().unwrap();
+        sleep(Duration::from_millis(1));
+    }
 }
